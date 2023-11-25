1e989b8375cb89a27fdbb6b7831167aea2aaefe6
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-25 13:38:11 -0300

    References in vue

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -6,15 +6,16 @@ import { Link } from '@inertiajs/vue3'
 defineProps({
     objectList: Object,
     deleteRoute: String,
+    edit : false
 });
 
 </script>
 
 <template>
-    <input v-if="edit"  v-model="editedElement" placeholder="current.name" />
+    <!-- <input v-if="edit" placeholder="current.name" /> -->
     <p v-if="!edit">{{ (current.name ? current.name : current.title) }}</p>
     <div class="float-right inline-block">
-        <button v-on:click="$event => edit = !edit" class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800">Edit</button>
+        <button v-on:click="console.log(this.$refs)" class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800">Edit</button>
         <Link v-if="deleteRoute" method="delete" as="button" type="button" :href="route(deleteRoute, current, )" class="bg-red-600 text-white p-1 rounded-md hover:bg-red-800">Delete</Link>
     </div>
 </template>
--- resources/js/Components/CardList.vue
+++ resources/js/Components/CardList.vue
@@ -21,13 +21,13 @@ defineExpose({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10">
-                    <div class="bg-gray-200 m-2 p-4 rounded-md" v-for="(current, index) in objectList">                     
-                        <input v-if="edit" v-model="editedElement" :placeholder="current.name" />
-                        <p v-if="!edit">{{ (current.name ? current.name : current.title) }}</p>
-                        <!-- <div class="float-right inline-block">
-                            <button v-on:click="$event => edit = !edit" class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800">Edit</button>
+                    <div class="bg-gray-200 m-2 p-4 rounded-md" v-for="(current, index) in objectList" :key="current.id" ref="div">                     
+                        <input ref="input" v-if="edit" v-model="editedElement" :placeholder="current.name" />
+                        <p v-if="!edit" ref="p">{{ (current.name ? current.name : current.title) }}</p>
+                        <div class="float-right inline-block">
+                            <button v-on:click="$event => console.log(this.$refs.div[index])" class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800">Edit</button>
                             <Link v-if="deleteRoute" method="delete" as="button" type="button" :href="route(deleteRoute, current, )" class="bg-red-600 text-white p-1 rounded-md hover:bg-red-800">Delete</Link>
-                        </div> -->
+                        </div>
                     </div>
                 </div>
             </div>
