3f9a1d835402869cd3a3704e06afc5fb118a347c
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-25 07:43:14 -0300

    Edit elemetn

--- resources/js/Components/CardList.vue
+++ resources/js/Components/CardList.vue
@@ -2,26 +2,32 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { router } from '@inertiajs/vue3'
 import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
-defineProps({
+const props = defineProps({
     objectList: Object,
     deleteRoute: String,
+    
 });
 
+const edit = true
 
+defineExpose({
+  edit
+})
 </script>
 
 <template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10">
-                    <div class="bg-gray-200 m-2 p-4 rounded-md" v-for="current in objectList">                       
-                        <input v-if="edit" v-model="editedElement" placeholder="current.name" />
+                    <div class="bg-gray-200 m-2 p-4 rounded-md" v-for="(current, index) in objectList">                     
+                        <input v-if="edit" v-model="editedElement" :placeholder="current.name" />
                         <p v-if="!edit">{{ (current.name ? current.name : current.title) }}</p>
-                        <div class="float-right inline-block">
+                        <!-- <div class="float-right inline-block">
                             <button v-on:click="$event => edit = !edit" class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800">Edit</button>
                             <Link v-if="deleteRoute" method="delete" as="button" type="button" :href="route(deleteRoute, current, )" class="bg-red-600 text-white p-1 rounded-md hover:bg-red-800">Delete</Link>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
