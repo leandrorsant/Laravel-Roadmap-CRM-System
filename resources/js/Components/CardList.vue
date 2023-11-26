1a4505fd5ef8738921fbf780b2f5938c214200e1
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-25 21:21:18 -0300

    Toggle element

--- resources/js/Components/CardList.vue
+++ resources/js/Components/CardList.vue
@@ -21,11 +21,20 @@ defineExpose({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10">
-                    <div class="bg-gray-200 m-2 p-4 rounded-md" v-for="(current, index) in objectList" :key="current.id" ref="div">                     
-                        <input ref="input" v-if="edit" v-model="editedElement" :placeholder="current.name" />
-                        <p v-if="!edit" ref="p">{{ (current.name ? current.name : current.title) }}</p>
+                    <div :id="index" class="bg-gray-200 m-2 p-4 rounded-md" v-for="(current, index) in objectList" :key="current.id">                     
+                        <input :ref="'input'+current.id" class="hidden" :placeholder="current.name" :value="current.name" />
+                        <p :id="'p'+current.id" :ref="'p'+current.id">{{ (current.name ? current.name : current.title) }}</p>
                         <div class="float-right inline-block">
-                            <button v-on:click="$event => console.log(this.$refs.div[index])" class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800">Edit</button>
+
+                            <button 
+                                v-on:click="$event => {
+                                    this.$refs['input'+current.id][0].classList.toggle('hidden')
+                                    this.$refs['p'+current.id][0].classList.toggle('hidden')
+                                }" 
+                                class="bg-blue-400 text-white mr-2 p-1 rounded-md hover:bg-blue-800"
+                            >
+                                Edit
+                            </button>
                             <Link v-if="deleteRoute" method="delete" as="button" type="button" :href="route(deleteRoute, current, )" class="bg-red-600 text-white p-1 rounded-md hover:bg-red-800">Delete</Link>
                         </div>
                     </div>
